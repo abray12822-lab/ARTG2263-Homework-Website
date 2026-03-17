3553af87fb8c054aaf3f3c0cd969aecada695fa9
abray12822-lab <abray12822@gmail.com>
2026-03-17 15:30:28 -0400

    Update sketch.js

--- assignment-1/sketch.js
+++ assignment-1/sketch.js
@@ -1,17 +1,9 @@
 function setup() {
-  // Create a canvas as big as the container window
-  createCanvas(windowWidth, windowHeight);
-  textAlign(CENTER, CENTER);
+  // This line immediately tells the browser to change the URL
+  window.location.href = "https://studio.code.org/c/2771787518";
 }
 
 function draw() {
-  background(220);
-  textSize(height / 16);
-  text("https://studio.code.org/c/2771787518", width / 2, height / 2);
-}
-
-// This function is called any time the window is resized
-function windowResized() {
-  // Change the canvas size to fit the window
-  resizeCanvas(windowWidth, windowHeight);
+  // We leave this empty because the page will redirect 
+  // before anything even has a chance to be drawn.
 }
